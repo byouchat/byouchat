dd3823b32e6853dbe5b67bfabae7739dcbf8eaca
Yuku Kotani <yukukotani@gmail.com>
2024-05-21 00:45:44 +0900

    add llm reply

--- routes/api/messages.ts
+++ routes/api/messages.ts
@@ -13,6 +13,36 @@ export const handler: Handlers = {
       expireIn: 5000,
     });
 
+    setTimeout(async () => {
+      const res = await fetch("https://openrouter.ai/api/v1/chat/completions", {
+        method: "POST",
+        headers: {
+          "Authorization": `Bearer ${Deno.env.get("OPEN_ROUTER_API_TOKEN")}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          "model": "meta-llama/llama-3-8b-instruct:free",
+          "messages": [
+            {
+              "role": "system",
+              "content":
+                "ここはインターネットのチャット掲示板です。メッセージに対して30文字以内の日本語で返事してください。2chのような口調で、少しぶっきらぼうで、タメ口で答えてください。",
+            },
+            { "role": "user", "content": body.message },
+          ],
+        }),
+      });
+
+      const llmBody = await res.json();
+      const llmMessage = llmBody?.choices?.at(0)?.message?.content;
+      if (llmMessage) {
+        const id = crypto.randomUUID();
+        kv.set(["latestMessage"], { id, message: llmMessage }, {
+          expireIn: 5000,
+        });
+      }
+    }, 1500);
+
     return new Response(JSON.stringify({ ok: "ok" }), {
       headers: { "Content-Type": "application/json" },
     });
