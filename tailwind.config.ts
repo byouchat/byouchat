72c182073f31f6f808342b1cdde33318ed7e2219
Yuku Kotani <yukukotani@gmail.com>
2024-05-26 20:46:53 +0900

    src dir

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,6 @@ import { type Config } from "tailwindcss";
 
 export default {
   content: [
-    "{routes,islands,components}/**/*.{ts,tsx}",
+    "src/{routes,islands,components}/**/*.{ts,tsx}",
   ],
 } satisfies Config;
